a13e7901f4408e24a119a3b0c68484825d84bc0d
hnvadobe <hnv@adobe.com>
2025-12-16 15:04:04 +0530

    change

--- event-libs/v1/features/timing-framework/worker-traditional.js
+++ event-libs/v1/features/timing-framework/worker-traditional.js
@@ -254,7 +254,7 @@ class TimingWorker {
       // Convert toggleTime to number if it's a string, like in shouldTriggerNextSchedule
       const numericToggleTime = typeof t === 'string' ? parseInt(t, 10) : t;
       const toggleTimePassed = typeof numericToggleTime !== 'number' || adjustedTime > numericToggleTime;
-      console.log('adjustedTime > numericToggleTime', adjustedTime > numericToggleTime, start);
+      console.log('adjustedTime > numericToggleTime', adjustedTime , numericToggleTime, start);
       if (!toggleTimePassed) break;
 
       start = pointer;
